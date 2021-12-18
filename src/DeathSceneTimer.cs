dd06cd70cba602d7c5cfacf0457446af44690212
Dominick <dominick.serwe@gmail.com>
2021-12-17 19:05:18 -0500

    cleaned up the repository

---
+++ src/DeathSceneTimer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathSceneTimer : MonoBehaviour
+{
+    public float timing;
+    public float startcountdown = 10f;
+
+    public GameObject YouDied;
+    public GameObject Survived;
+    public GameObject Trash;
+    public GameObject FinalScore;
+    public GameObject PlayAgainQuit;
+    public float trashPicked;
+
+
+    public Text timeText;
+    public Text scoreText;
+    public Text trashText;
+
+    void Start()
+    {
+        AudioManager.singleton.Play("PlayerDeath");
+        trashPicked = (Timer.singleton.score - (Timer.singleton.totalTime/10)) / 100;
+        YouDied.SetActive(false);
+        Survived.SetActive(false);
+        Trash.SetActive(false);
+        FinalScore.SetActive(false);
+        PlayAgainQuit.SetActive(false);
+        timing = startcountdown;
+        trashText.text = "You  Picked   Up   " + trashPicked.ToString("F0") + "   Pieces   of   Trash";
+        scoreText.text = "Your Score was:   " + Timer.singleton.score.ToString();
+        timeText.text = "You   Survived   for:   " + Timer.singleton.minutes.ToString("00") + ":" + Timer.singleton.seconds.ToString("00");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (timing <= 8){
+            YouDied.SetActive(true);
+
+        }
+        if (timing <= 6){
+            Survived.SetActive(true);
+
+        }
+        if (timing <= 4){
+            Trash.SetActive(true);
+
+        }
+        if (timing <= 2){
+            FinalScore.SetActive(true);
+
+        }
+        if (timing <=0){
+            PlayAgainQuit.SetActive(true);
+        }
+
+        timing -= Time.deltaTime;
+    }
+}
---
+++ src/Trash.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trash : MonoBehaviour
+{
+    private Rigidbody2D body = null;
+    private Animator anim = null;
+
+    public float fallTargetTime = 0.0f;
+    public float numKeyFrames = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
+        anim.speed = 0;
+
+        numKeyFrames = (Random.Range(0, numKeyFrames - 1) / 10);
+        anim.Play("trash", 0, numKeyFrames);
+
+        fallTargetTime = Random.Range(5.0f, 20.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fallTargetTime -= Time.deltaTime;
+        if (fallTargetTime <= 0.0f) {
+            body.gravityScale = 0.0f;
+            body.velocity = Vector2.zero;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Timer.singleton.score += 100.0f;  
+        AudioManager.singleton.Play("ItemPickup");
+        Destroy(gameObject);
+    }
+}
